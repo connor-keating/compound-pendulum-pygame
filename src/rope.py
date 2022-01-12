b4aade12f29db084e589c0e4679704ebd8d5458e
Connor Keating <conkeating32@gmail.com>
2022-01-11 18:18:07 -0600

    [STYLE] Formatting code with black

--- src/rope.py
+++ src/rope.py
@@ -1,9 +1,12 @@
 import numpy as np
 
+
 class rope:
     """A class containing necessary information and behaviors for the rope (lines) of the simulation."""
 
-    def __init__(self, color = (255, 255, 255), width=2, length = 100, theta = np.pi/4, start = [0, 0]):
+    def __init__(
+        self, color=(255, 255, 255), width=2, length=100, theta=np.pi / 4, start=[0, 0]
+    ):
         self.color = color
         self.width = width
         self._length = length
@@ -21,7 +24,7 @@ class rope:
 
     @property
     def start(self):
-        return self._start # an underscore denotes private variables
+        return self._start  # an underscore denotes private variables
 
     @property
     def end(self):
@@ -34,15 +37,15 @@ class rope:
     @start.setter
     def start(self, new_start):
         if len(new_start) != 2:
-            raise ValueError('Starting point must be a list of 2 X, Y coordinates')
+            raise ValueError("Starting point must be a list of 2 X, Y coordinates")
         self._start = np.abs(new_start)
-    
+
     @end.setter
     def end(self, new_end):
         if len(new_end) != 2:
-            raise ValueError('Ending point must be a list of 2 X, Y coordinates')
+            raise ValueError("Ending point must be a list of 2 X, Y coordinates")
         self._end = np.abs(new_end)
-    
+
     def swing(self):
         x = np.cos(self.theta) * self.length + self.start[0]
         y = np.sin(self.theta) * self.length + self.start[1]
